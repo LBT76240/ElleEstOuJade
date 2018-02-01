b67013401a63a9047b49801690969ae5a22de5f7
LBT76240 <trailin_88_@hotmail.fr>
2018-02-02 00:14:53 +0100

    Matchmaking de groupe de 2 ok

--- src/firstAgent/HelloWorldAgent.java
+++ src/firstAgent/HelloWorldAgent.java
@@ -21,6 +21,7 @@ public class HelloWorldAgent extends Agent {
     class Stats {
         int age;
         int score;
+        boolean findGame;
     }
 
     Stats ownStats = new Stats();
@@ -32,7 +33,7 @@ public class HelloWorldAgent extends Agent {
     int me;
 
     protected void setup() {
-        System.out.println("Hello World! My name is "+getLocalName());
+
         me = Integer.valueOf(getLocalName());
 
 
@@ -47,6 +48,12 @@ public class HelloWorldAgent extends Agent {
         int maxScore = 10000;
         int minScore = 0;
         ownStats.score = ThreadLocalRandom.current().nextInt(minScore, maxScore + 1);
+        ownStats.findGame = false;
+        System.out.println("Hello World! My name is "+getLocalName() + " Age : " + ownStats.age + " Score : " + ownStats.score);
+        final int[] ageDifference = {10};
+        int ageInc = 10;
+        final int[] scoreDifference = {1000};
+        int scoreInc = 1000;
 
         for(int i = 0;i<maxAgent;i++) {
             AID idI = new AID(String.valueOf(i),AID.ISLOCALNAME);
@@ -61,43 +68,121 @@ public class HelloWorldAgent extends Agent {
             msgScore.setOntology("SCORE");
             msgScore.setContent(String.valueOf(ownStats.score));
             send(msgScore);
+
+            ACLMessage msgFindGame = new ACLMessage(ACLMessage.INFORM);
+            msgFindGame.addReceiver(idI);
+            msgFindGame.setOntology("findGame");
+            msgFindGame.setContent(String.valueOf(ownStats.findGame));
+            send(msgFindGame);
+
             listStatsPlayer.add(new Stats());
         }
 
 
+
+        final int[] j = {1};
         //doDelete();
 
         addBehaviour(new TickerBehaviour(this,1000) {
             @Override
             protected void onTick() {
+                if(!ownStats.findGame) {
+                    System.out.print(".");
+                    ACLMessage msg = receive();
+                    if (msg != null) {
+                        while (msg != null) {
+                            if(!ownStats.findGame) {
 
-                ACLMessage msg = receive();
-                if(msg != null) {
-                    while(msg != null) {
-                        if(msg.getOntology().equals("AGE")) {
-                            int i = Integer.parseInt(msg.getSender().getLocalName());
-                            listStatsPlayer.get(i).age = Integer.parseInt(msg.getContent());
+                                int indice = -1;
+
+                                String[] sender = msg.getSender().getName().split("@");
+                                indice = Integer.parseInt(sender[0]);
+                                if (msg.getOntology().equals("AGE")) {
+                                    listStatsPlayer.get(indice).age = Integer.parseInt(msg.getContent());
+                                } else if (msg.getOntology().equals("SCORE")) {
+                                    listStatsPlayer.get(indice).score = Integer.parseInt(msg.getContent());
+                                } else if (msg.getOntology().equals("findGame")) {
+                                    if (msg.getContent().equals("true")) {
+                                        listStatsPlayer.get(indice).findGame = true;
+                                    } else {
+                                        listStatsPlayer.get(indice).findGame = false;
+                                    }
+                                } else if (msg.getOntology().equals("REQUEST")) {
+                                    if (msg.getContent().equals("PLAY?")) {
+                                        if (Math.abs(listStatsPlayer.get(indice).age - ownStats.age) < ageDifference[0] && Math.abs(listStatsPlayer.get(indice).score - ownStats.score) < scoreDifference[0]) {
+                                            AID idI = new AID(String.valueOf(indice), AID.ISLOCALNAME);
+                                            ACLMessage msgScore = new ACLMessage(ACLMessage.INFORM);
+                                            msgScore.addReceiver(idI);
+                                            msgScore.setOntology("REQUEST");
+                                            msgScore.setContent("OK");
+                                            send(msgScore);
+                                        }
+                                    } else {
+                                        if (msg.getContent().equals("OK")) {
+                                            AID idI = new AID(String.valueOf(indice), AID.ISLOCALNAME);
+                                            ACLMessage msgScore = new ACLMessage(ACLMessage.INFORM);
+                                            msgScore.addReceiver(idI);
+                                            msgScore.setOntology("REQUEST");
+                                            msgScore.setContent("OK");
+                                            send(msgScore);
+
+                                            for (int i = 0; i < maxAgent; i++) {
+                                                if (i != me && !listStatsPlayer.get(i).findGame) {
+
+
+                                                    ACLMessage msgFindGame = new ACLMessage(ACLMessage.INFORM);
+                                                    msgFindGame.addReceiver(idI);
+                                                    msgFindGame.setOntology("findGame");
+                                                    msgFindGame.setContent(String.valueOf(true));
+                                                    send(msgFindGame);
+
+                                                    listStatsPlayer.add(new Stats());
+                                                }
+                                            }
+                                            System.out.println("\n" + me + "+" + indice);
+                                            ownStats.findGame = true;
+                                        }
+                                    }
+
+                                }
+
+                            }
+
+
+                            msg = receive();
                         }
 
-                        if(msg.getOntology().equals("SCORE")) {
-                            int i = Integer.parseInt(msg.getSender().getLocalName());
-                            listStatsPlayer.get(i).score = Integer.parseInt(msg.getContent());
+
+                    } else {
+
+                        if (j[0] % 10 == 0) {
+                            ageDifference[0] = ageDifference[0] + ageInc;
+                            scoreDifference[0] = scoreDifference[0] + scoreInc;
                         }
-                        msg = receive();
+
+
+                        for (int i = 0; i < maxAgent; i++) {
+                            if (i != me && !listStatsPlayer.get(i).findGame) {
+
+                                if (Math.abs(listStatsPlayer.get(i).age - ownStats.age) < ageDifference[0] && Math.abs(listStatsPlayer.get(i).score - ownStats.score) < scoreDifference[0]) {
+
+                                    AID idI = new AID(String.valueOf(i), AID.ISLOCALNAME);
+                                    ACLMessage msgScore = new ACLMessage(ACLMessage.INFORM);
+                                    msgScore.addReceiver(idI);
+                                    msgScore.setOntology("REQUEST");
+                                    msgScore.setContent("PLAY?");
+                                    send(msgScore);
+                                }
+
+                            }
+                        }
+
+                        j[0] = j[0] + 1;
                     }
-
-
                 } else {
-                    System.out.println("The stats that i know is : ");
-                    for(int i = 0;i<maxAgent;i++) {
-                        if(me == i) {
-                            System.out.println("me <3<3 : ");
-                        } else {
-                            System.out.println("Agents "+i+" : ");
-                        }
-
-                        System.out.println("Age : " + listStatsPlayer.get(i).age);
-                        System.out.println("Score : " +listStatsPlayer.get(i).score);
+                    ACLMessage msg = receive();
+                    if (msg != null) {
+                        msg = receive();
                     }
                 }
 
